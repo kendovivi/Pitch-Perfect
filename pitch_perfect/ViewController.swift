9b88c5d99eaf075cc0e36aa9360c29d1f84fef03
Zheng Wang <zheng_wang@timebank.co.jp>
2020-07-05 15:39:50 +0900

    init commit

---
+++ pitch_perfect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pitch_perfect
+//
+//  Created by 王峥 on 2020/07/05.
+//  Copyright © 2020 kendovivi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
