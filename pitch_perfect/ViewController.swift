295107f3da0e4d4b5b72e803bcdc704374cf67d9
Zheng Wang <zheng_wang@timebank.co.jp>
2020-07-06 23:27:52 +0900

    add record, stop buttons, record label and add contraints

--- pitch_perfect/ViewController.swift
+++ pitch_perfect/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func recordButton(_ sender: Any) {
+        print("test record pressed")
+        recordingLabel.text = "recording in progress"
+    }
+    
+    @IBAction func stopRecordButton(_ sender: Any) {
+        print("stop record")
+    }
 }
 
